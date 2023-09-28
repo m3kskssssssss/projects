0a06d06d41855072bb54ed8d114da2d04ff76fe2
Mickle <misha_2003_5@mail.ru>
2023-09-28 16:26:50 +0500

    Добавил версию 2.0 приложения для математических вычислений
    Большое обновление программы для мат. вычислений:
    1)Переработана функция ввода действия
    2)Добавлено предупреждение о неправильном вводе данных
    3)Добавлено деление двух чисел с точностью до сотых

--- Strelnikov_01.py
+++ Strelnikov_01.py
@@ -1,12 +1,41 @@
+pervoe_chislo_modul = 0
+vtoroe_chislo_modul = 0
+chastnoe = 0
+
+
+
 print('Введите первое число:')
 pervoe_chislo = int(input())
 print('Введите второе число:')
 vtoroe_chislo = int(input())
-print('Сумма:' + str(pervoe_chislo + vtoroe_chislo))
-print('Разность:' + str(pervoe_chislo - vtoroe_chislo))
-print('Произведение:' + str(pervoe_chislo * vtoroe_chislo))
-print('Среднее арифметическое:' + str((pervoe_chislo + vtoroe_chislo)/2))
-if pervoe_chislo > vtoroe_chislo:
-    print('Разность максимума и минимума:' + str(pervoe_chislo - vtoroe_chislo))
+print('Введите математический знак: +, -, *, /, srednee_arifm, raznica_moduley')
+deystviye = input()
+
+
+
+if deystviye == '+':
+    print('Сумма двух чисел: ' + str(pervoe_chislo + vtoroe_chislo))
+elif deystviye == '-':
+    print('Разность двух чисел: ' + str(pervoe_chislo - vtoroe_chislo))
+elif deystviye == '*':
+    print('Произведение двух чисел: ' + str(pervoe_chislo * vtoroe_chislo))
+elif deystviye == 'srednee_arifm':
+    print('Среднее арифметическое двух чисел: ' + str((pervoe_chislo + vtoroe_chislo)/2))
+elif deystviye == 'raznica_moduley':
+    if pervoe_chislo >= 0:
+        pervoe_chislo_modul = pervoe_chislo
+    else:
+        pervoe_chislo_modul = - pervoe_chislo
+    if vtoroe_chislo >= 0:
+        vtoroe_chislo_modul = vtoroe_chislo
+    else:
+        vtoroe_chislo_modul = - vtoroe_chislo
+    if pervoe_chislo_modul >= vtoroe_chislo_modul:
+        print('Разница модулей двух чисел равна: ' + str(pervoe_chislo_modul - vtoroe_chislo_modul))
+    else:
+        print('Разница модулей двух чисел равна: ' + str(vtoroe_chislo_modul - pervoe_chislo_modul))
+elif deystviye == '/':
+    chastnoe = pervoe_chislo / vtoroe_chislo
+    print(f"{chastnoe:.2f}")
 else:
-    print('Разность максимума и минимума:' + str(vtoroe_chislo - pervoe_chislo))
+    print('Неверно указано необходимое математическое действие или число. Перезапустите программу.')
